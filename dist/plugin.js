1b080fa738c65f878b94e4a9a8a6cf1f8c23b6a4
Win_7 <90262580+W-i-n-7@users.noreply.github.com>
2025-01-23 23:03:32 +0200

    Create plugin.js

---
+++ dist/plugin.js
@@ -0,0 +1,29 @@
+exports.version = 1.0
+exports.description = "Provides an url that returns the IP of the user"
+exports.apiRequired = 9.6
+exports.repo = "W-i-n-7/getip-api"
+
+exports.config = {
+    apiUrl: {
+        frontend: false,
+        type: 'string',
+        multiple: false,
+        label: "Url for the api.",
+        helperText: "Default: /getip\nRESTART PLUGIN TO APPLY!",
+        defaultValue: "/getip"
+    },
+}
+
+var apiPath = "/getip"
+
+exports.init = async api => {
+    apiPath = api.getConfig('apiUrl')
+}
+
+exports.middleware = ctx => {
+    if (ctx.path === apiPath)
+    {
+        ctx.body = ctx.ip
+        return ctx.stop?.() || true
+    }
+}
